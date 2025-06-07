5d030f5a879fbccfb6031fed65a3d4ccb759cb71
muhammadpadanta <dantagaul22@gmail.com>
2025-06-07 20:25:09 +0700

    update brewfile & vscode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -92,7 +92,7 @@ alias brewupdate='brew update && brew upgrade && brew cleanup'
 set -U fish_history_limit 5000
 
 # FZF config
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
 
 
 fzf_configure_bindings --directory=\cf
